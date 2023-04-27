715e0752ff88699f080d7746bb8e9aec8e2ccd7f
ousttrue <ousttrue@gmail.com>
2023-04-27 14:44:12 +0900

    upper

--- include/grapho/orbitview.h
+++ include/grapho/orbitview.h
@@ -4,39 +4,39 @@
 #include <cmath>
 
 namespace grapho {
-class OrbitView
-{
-  float fovY_ = DirectX::XMConvertToRadians(30.0f);
-  float nearZ_ = 0.01f;
-  float farZ_ = 1000.0f;
 
-  float yaw_ = {};
-  float pitch_ = {};
+struct OrbitView
+{
+  int Width = 1;
+  int Height = 1;
+  float FovY = DirectX::XMConvertToRadians(30.0f);
+  float NearZ = 0.01f;
+  float FarZ = 1000.0f;
+
+  float Yaw = {};
+  float Pitch = {};
   float shift_[3] = { 0, -0.8f, -5 };
 
-public:
-  int width_ = 1;
-  int height_ = 1;
   OrbitView() {}
 
   void SetSize(int w, int h)
   {
-    if (w == width_ && h == height_) {
+    if (w == Width && h == Height) {
       return;
     }
-    width_ = w;
-    height_ = h;
+    Width = w;
+    Height = h;
   }
 
   void YawPitch(int dx, int dy)
   {
-    yaw_ += DirectX::XMConvertToRadians(static_cast<float>(dx));
-    pitch_ += DirectX::XMConvertToRadians(static_cast<float>(dy));
+    Yaw += DirectX::XMConvertToRadians(static_cast<float>(dx));
+    Pitch += DirectX::XMConvertToRadians(static_cast<float>(dy));
   }
 
   void Shift(int dx, int dy)
   {
-    auto factor = std::tan(fovY_ * 0.5f) * 2.0f * shift_[2] / height_;
+    auto factor = std::tan(FovY * 0.5f) * 2.0f * shift_[2] / Height;
     shift_[0] -= dx * factor;
     shift_[1] += dy * factor;
   }
@@ -52,35 +52,35 @@ public:
 
   void Update(const float projection[16], const float view[16]) const
   {
-    float aspectRatio = (float)width_ / (float)height_;
+    float aspectRatio = (float)Width / (float)Height;
     DirectX::XMStoreFloat4x4(
       (DirectX::XMFLOAT4X4*)projection,
-      DirectX::XMMatrixPerspectiveFovRH(fovY_, aspectRatio, nearZ_, farZ_));
+      DirectX::XMMatrixPerspectiveFovRH(FovY, aspectRatio, NearZ, FarZ));
 
-    auto yaw = DirectX::XMMatrixRotationY(yaw_);
-    auto pitch = DirectX::XMMatrixRotationX(pitch_);
+    auto yaw = DirectX::XMMatrixRotationY(Yaw);
+    auto pitch = DirectX::XMMatrixRotationX(Pitch);
     auto shift = DirectX::XMMatrixTranslation(shift_[0], shift_[1], shift_[2]);
     DirectX::XMStoreFloat4x4((DirectX::XMFLOAT4X4*)view, yaw * pitch * shift);
   }
 
   void Fit(const DirectX::XMFLOAT3& min, const DirectX::XMFLOAT3& max)
   {
-    yaw_ = {};
-    pitch_ = {};
+    Yaw = {};
+    Pitch = {};
     // shift_[3] = { 0, -0.8f, -5 };
     auto height = max.y - min.y;
     if (fabs(height) < 1e-4) {
       return;
     }
-    auto distance = height * 0.5f / std::atan(fovY_ * 0.5f);
+    auto distance = height * 0.5f / std::atan(FovY * 0.5f);
     shift_[0] = 0;
     shift_[1] = -height * 0.5f;
     shift_[2] = -distance * 1.2f;
     auto r =
       DirectX::XMVectorGetX(DirectX::XMVector3Length(DirectX::XMVectorSubtract(
         DirectX::XMLoadFloat3(&min), DirectX::XMLoadFloat3(&max))));
-    nearZ_ = r * 0.01f;
-    farZ_ = r * 100.0f;
+    NearZ = r * 0.01f;
+    FarZ = r * 100.0f;
   }
 };
 }
