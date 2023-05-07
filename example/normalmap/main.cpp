e91141dd07587b34957ce3f93ed1930d15073a95
ousttrue <ousttrue@gmail.com>
2023-05-07 16:56:08 +0900

    dockspace

--- example/normalmap/main.cpp
+++ example/normalmap/main.cpp
@@ -7,6 +7,7 @@
 #include <glm/gtc/matrix_transform.hpp>
 #include <grapho/gl3/shader.h>
 #include <grapho/gl3/shadergenerator.h>
+#include <grapho/imgui/dockspace.h>
 #include <grapho/orbitview.h>
 #include <grapho/shadersnippet.h>
 #include <imgui.h>
@@ -98,163 +99,6 @@ processInput(GLFWwindow* window)
 }
 
 auto DOCK_SPACE = "DOCK_SPACE";
-void
-DockSpace()
-{
-  // If you strip some features of, this demo is pretty much equivalent to
-  // calling DockSpaceOverViewport()! In most cases you should be able to just
-  // call DockSpaceOverViewport() and ignore all the code below! In this
-  // specific demo, we are not using DockSpaceOverViewport() because:
-  // - we allow the host window to be floating/moveable instead of filling the
-  // viewport (when opt_fullscreen == false)
-  // - we allow the host window to have padding (when opt_padding == true)
-  // - we have a local menu bar in the host window (vs. you could use
-  // BeginMainMenuBar() + DockSpaceOverViewport() in your code!) TL;DR; this
-  // demo is more complicated than what you would normally use. If we removed
-  // all the options we are showcasing, this demo would become:
-  //     void ShowExampleAppDockSpace()
-  //     {
-  //         ImGui::DockSpaceOverViewport(ImGui::GetMainViewport());
-  //     }
-
-  static bool opt_fullscreen = true;
-  static bool opt_padding = false;
-  static ImGuiDockNodeFlags dockspace_flags =
-    ImGuiDockNodeFlags_PassthruCentralNode;
-
-  // We are using the ImGuiWindowFlags_NoDocking flag to make the parent window
-  // not dockable into, because it would be confusing to have two docking
-  // targets within each others.
-  ImGuiWindowFlags window_flags =
-    ImGuiWindowFlags_MenuBar | ImGuiWindowFlags_NoDocking;
-  if (opt_fullscreen) {
-    const ImGuiViewport* viewport = ImGui::GetMainViewport();
-    ImGui::SetNextWindowPos(viewport->WorkPos);
-    ImGui::SetNextWindowSize(viewport->WorkSize);
-    ImGui::SetNextWindowViewport(viewport->ID);
-    ImGui::PushStyleVar(ImGuiStyleVar_WindowRounding, 0.0f);
-    ImGui::PushStyleVar(ImGuiStyleVar_WindowBorderSize, 0.0f);
-    window_flags |= ImGuiWindowFlags_NoTitleBar | ImGuiWindowFlags_NoCollapse |
-                    ImGuiWindowFlags_NoResize | ImGuiWindowFlags_NoMove;
-    window_flags |=
-      ImGuiWindowFlags_NoBringToFrontOnFocus | ImGuiWindowFlags_NoNavFocus;
-  } else {
-    dockspace_flags &= ~ImGuiDockNodeFlags_PassthruCentralNode;
-  }
-
-  // When using ImGuiDockNodeFlags_PassthruCentralNode, DockSpace() will render
-  // our background and handle the pass-thru hole, so we ask Begin() to not
-  // render a background.
-  if (dockspace_flags & ImGuiDockNodeFlags_PassthruCentralNode)
-    window_flags |= ImGuiWindowFlags_NoBackground;
-
-  // Important: note that we proceed even if Begin() returns false (aka window
-  // is collapsed). This is because we want to keep our DockSpace() active. If a
-  // DockSpace() is inactive, all active windows docked into it will lose their
-  // parent and become undocked. We cannot preserve the docking relationship
-  // between an active window and an inactive docking, otherwise any change of
-  // dockspace/settings would lead to windows being stuck in limbo and never
-  // being visible.
-  if (!opt_padding)
-    ImGui::PushStyleVar(ImGuiStyleVar_WindowPadding, ImVec2(0.0f, 0.0f));
-  ImGui::Begin(DOCK_SPACE, nullptr, window_flags);
-  if (!opt_padding)
-    ImGui::PopStyleVar();
-
-  if (opt_fullscreen)
-    ImGui::PopStyleVar(2);
-
-  // Submit the DockSpace
-  ImGuiIO& io = ImGui::GetIO();
-  if (io.ConfigFlags & ImGuiConfigFlags_DockingEnable) {
-    ImGuiID dockspace_id = ImGui::GetID(DOCK_SPACE);
-
-    if (!ImGui::DockBuilderGetNode(dockspace_id)) {
-
-      ImGui::DockBuilderRemoveNode(dockspace_id);
-      ImGui::DockBuilderAddNode(dockspace_id);
-
-      // Build dockspace...
-
-      ImGui::DockBuilderFinish(dockspace_id);
-    }
-
-    ImGui::DockSpace(dockspace_id, ImVec2(0.0f, 0.0f), dockspace_flags);
-  } else {
-    assert(false);
-  }
-
-  if (ImGui::BeginMenuBar()) {
-    if (ImGui::BeginMenu("Options")) {
-      // Disabling fullscreen would allow the window to be moved to the front of
-      // other windows, which we can't undo at the moment without finer window
-      // depth/z control.
-      ImGui::MenuItem("Fullscreen", nullptr, &opt_fullscreen);
-      ImGui::MenuItem("Padding", nullptr, &opt_padding);
-      ImGui::Separator();
-
-      if (ImGui::MenuItem("Flag: NoSplit",
-                          "",
-                          (dockspace_flags & ImGuiDockNodeFlags_NoSplit) !=
-                            0)) {
-        dockspace_flags ^= ImGuiDockNodeFlags_NoSplit;
-      }
-      if (ImGui::MenuItem("Flag: NoResize",
-                          "",
-                          (dockspace_flags & ImGuiDockNodeFlags_NoResize) !=
-                            0)) {
-        dockspace_flags ^= ImGuiDockNodeFlags_NoResize;
-      }
-      if (ImGui::MenuItem("Flag: NoDockingInCentralNode",
-                          "",
-                          (dockspace_flags &
-                           ImGuiDockNodeFlags_NoDockingInCentralNode) != 0)) {
-        dockspace_flags ^= ImGuiDockNodeFlags_NoDockingInCentralNode;
-      }
-      if (ImGui::MenuItem(
-            "Flag: AutoHideTabBar",
-            "",
-            (dockspace_flags & ImGuiDockNodeFlags_AutoHideTabBar) != 0)) {
-        dockspace_flags ^= ImGuiDockNodeFlags_AutoHideTabBar;
-      }
-      if (ImGui::MenuItem(
-            "Flag: PassthruCentralNode",
-            "",
-            (dockspace_flags & ImGuiDockNodeFlags_PassthruCentralNode) != 0,
-            opt_fullscreen)) {
-        dockspace_flags ^= ImGuiDockNodeFlags_PassthruCentralNode;
-      }
-      ImGui::Separator();
-
-      // if (ImGui::MenuItem("Close", nullptr, false, p_open != nullptr)){
-      //   *p_open = false;
-      // }
-      ImGui::EndMenu();
-    }
-
-    // if (ImGui::BeginMenu("Docks")) {
-    //   if (ImGui::MenuItem("Reset", "")) {
-    //     m_resetLayout = true;
-    //   }
-    //   ImGui::Separator();
-    //   // Disabling fullscreen would allow the window to be moved to the front of
-    //   // other windows, which we can't undo at the moment without finer window
-    //   // depth/z control.
-    //   for (auto& dock : Docks) {
-    //     ImGui::MenuItem(dock.Name.c_str(), nullptr, &dock.IsOpen);
-    //   }
-    //   ImGui::EndMenu();
-    // }
-
-    ImGui::EndMenuBar();
-  }
-
-  ImGui::End();
-
-  // for (auto& dock : Docks) {
-  //   dock.Show();
-  // }
-}
 
 int
 main(int argc, char** argv)
@@ -470,6 +314,13 @@ void main()
   // -------------
   glm::vec3 lightPos(0.5f, 1.0f, 0.3f);
 
+  std::vector<grapho::imgui::Dock> docks;
+
+  docks.push_back(
+    grapho::imgui::Dock("demo", [](const char* title, bool* popen) {
+      ImGui::ShowDemoWindow(popen);
+    }));
+
   // render loop
   // -----------
   while (!glfwWindowShouldClose(window)) {
@@ -480,12 +331,8 @@ void main()
     ImGui_ImplGlfw_NewFrame();
     ImGui::NewFrame();
 
-    DockSpace();
-
-    // 1. Show the big demo window (Most of the sample code is in
-    // ImGui::ShowDemoWindow()! You can browse its code to learn more about Dear
-    // ImGui!).
-    ImGui::ShowDemoWindow();
+    static bool resetLayout = false;
+    grapho::imgui::DockSpace("dock_space", docks, &resetLayout);
 
     processInput(window);
     int width, height;
