57079a33e50879959dfc0c7d25a3829dba00525d
ousttrue <ousttrue@gmail.com>
2023-05-07 13:43:24 +0900

    imgui

--- example/normalmap/main.cpp
+++ example/normalmap/main.cpp
@@ -9,6 +9,10 @@
 #include <grapho/gl3/shadergenerator.h>
 #include <grapho/orbitview.h>
 #include <grapho/shadersnippet.h>
+#include <imgui.h>
+#include <imgui_impl_glfw.h>
+#include <imgui_impl_opengl3.h>
+#include <imgui_internal.h>
 #include <iostream>
 
 const std::optional<bool> NOP = std::nullopt;
@@ -93,6 +97,165 @@ processInput(GLFWwindow* window)
   //   camera.ProcessKeyboard(RIGHT, deltaTime);
 }
 
+auto DOCK_SPACE = "DOCK_SPACE";
+void
+DockSpace()
+{
+  // If you strip some features of, this demo is pretty much equivalent to
+  // calling DockSpaceOverViewport()! In most cases you should be able to just
+  // call DockSpaceOverViewport() and ignore all the code below! In this
+  // specific demo, we are not using DockSpaceOverViewport() because:
+  // - we allow the host window to be floating/moveable instead of filling the
+  // viewport (when opt_fullscreen == false)
+  // - we allow the host window to have padding (when opt_padding == true)
+  // - we have a local menu bar in the host window (vs. you could use
+  // BeginMainMenuBar() + DockSpaceOverViewport() in your code!) TL;DR; this
+  // demo is more complicated than what you would normally use. If we removed
+  // all the options we are showcasing, this demo would become:
+  //     void ShowExampleAppDockSpace()
+  //     {
+  //         ImGui::DockSpaceOverViewport(ImGui::GetMainViewport());
+  //     }
+
+  static bool opt_fullscreen = true;
+  static bool opt_padding = false;
+  static ImGuiDockNodeFlags dockspace_flags =
+    ImGuiDockNodeFlags_PassthruCentralNode;
+
+  // We are using the ImGuiWindowFlags_NoDocking flag to make the parent window
+  // not dockable into, because it would be confusing to have two docking
+  // targets within each others.
+  ImGuiWindowFlags window_flags =
+    ImGuiWindowFlags_MenuBar | ImGuiWindowFlags_NoDocking;
+  if (opt_fullscreen) {
+    const ImGuiViewport* viewport = ImGui::GetMainViewport();
+    ImGui::SetNextWindowPos(viewport->WorkPos);
+    ImGui::SetNextWindowSize(viewport->WorkSize);
+    ImGui::SetNextWindowViewport(viewport->ID);
+    ImGui::PushStyleVar(ImGuiStyleVar_WindowRounding, 0.0f);
+    ImGui::PushStyleVar(ImGuiStyleVar_WindowBorderSize, 0.0f);
+    window_flags |= ImGuiWindowFlags_NoTitleBar | ImGuiWindowFlags_NoCollapse |
+                    ImGuiWindowFlags_NoResize | ImGuiWindowFlags_NoMove;
+    window_flags |=
+      ImGuiWindowFlags_NoBringToFrontOnFocus | ImGuiWindowFlags_NoNavFocus;
+  } else {
+    dockspace_flags &= ~ImGuiDockNodeFlags_PassthruCentralNode;
+  }
+
+  // When using ImGuiDockNodeFlags_PassthruCentralNode, DockSpace() will render
+  // our background and handle the pass-thru hole, so we ask Begin() to not
+  // render a background.
+  if (dockspace_flags & ImGuiDockNodeFlags_PassthruCentralNode)
+    window_flags |= ImGuiWindowFlags_NoBackground;
+
+  // Important: note that we proceed even if Begin() returns false (aka window
+  // is collapsed). This is because we want to keep our DockSpace() active. If a
+  // DockSpace() is inactive, all active windows docked into it will lose their
+  // parent and become undocked. We cannot preserve the docking relationship
+  // between an active window and an inactive docking, otherwise any change of
+  // dockspace/settings would lead to windows being stuck in limbo and never
+  // being visible.
+  if (!opt_padding)
+    ImGui::PushStyleVar(ImGuiStyleVar_WindowPadding, ImVec2(0.0f, 0.0f));
+  ImGui::Begin(DOCK_SPACE, nullptr, window_flags);
+  if (!opt_padding)
+    ImGui::PopStyleVar();
+
+  if (opt_fullscreen)
+    ImGui::PopStyleVar(2);
+
+  // Submit the DockSpace
+  ImGuiIO& io = ImGui::GetIO();
+  if (io.ConfigFlags & ImGuiConfigFlags_DockingEnable) {
+    ImGuiID dockspace_id = ImGui::GetID(DOCK_SPACE);
+
+    if (!ImGui::DockBuilderGetNode(dockspace_id)) {
+
+      ImGui::DockBuilderRemoveNode(dockspace_id);
+      ImGui::DockBuilderAddNode(dockspace_id);
+
+      // Build dockspace...
+
+      ImGui::DockBuilderFinish(dockspace_id);
+    }
+
+    ImGui::DockSpace(dockspace_id, ImVec2(0.0f, 0.0f), dockspace_flags);
+  } else {
+    assert(false);
+  }
+
+  if (ImGui::BeginMenuBar()) {
+    if (ImGui::BeginMenu("Options")) {
+      // Disabling fullscreen would allow the window to be moved to the front of
+      // other windows, which we can't undo at the moment without finer window
+      // depth/z control.
+      ImGui::MenuItem("Fullscreen", nullptr, &opt_fullscreen);
+      ImGui::MenuItem("Padding", nullptr, &opt_padding);
+      ImGui::Separator();
+
+      if (ImGui::MenuItem("Flag: NoSplit",
+                          "",
+                          (dockspace_flags & ImGuiDockNodeFlags_NoSplit) !=
+                            0)) {
+        dockspace_flags ^= ImGuiDockNodeFlags_NoSplit;
+      }
+      if (ImGui::MenuItem("Flag: NoResize",
+                          "",
+                          (dockspace_flags & ImGuiDockNodeFlags_NoResize) !=
+                            0)) {
+        dockspace_flags ^= ImGuiDockNodeFlags_NoResize;
+      }
+      if (ImGui::MenuItem("Flag: NoDockingInCentralNode",
+                          "",
+                          (dockspace_flags &
+                           ImGuiDockNodeFlags_NoDockingInCentralNode) != 0)) {
+        dockspace_flags ^= ImGuiDockNodeFlags_NoDockingInCentralNode;
+      }
+      if (ImGui::MenuItem(
+            "Flag: AutoHideTabBar",
+            "",
+            (dockspace_flags & ImGuiDockNodeFlags_AutoHideTabBar) != 0)) {
+        dockspace_flags ^= ImGuiDockNodeFlags_AutoHideTabBar;
+      }
+      if (ImGui::MenuItem(
+            "Flag: PassthruCentralNode",
+            "",
+            (dockspace_flags & ImGuiDockNodeFlags_PassthruCentralNode) != 0,
+            opt_fullscreen)) {
+        dockspace_flags ^= ImGuiDockNodeFlags_PassthruCentralNode;
+      }
+      ImGui::Separator();
+
+      // if (ImGui::MenuItem("Close", nullptr, false, p_open != nullptr)){
+      //   *p_open = false;
+      // }
+      ImGui::EndMenu();
+    }
+
+    // if (ImGui::BeginMenu("Docks")) {
+    //   if (ImGui::MenuItem("Reset", "")) {
+    //     m_resetLayout = true;
+    //   }
+    //   ImGui::Separator();
+    //   // Disabling fullscreen would allow the window to be moved to the front of
+    //   // other windows, which we can't undo at the moment without finer window
+    //   // depth/z control.
+    //   for (auto& dock : Docks) {
+    //     ImGui::MenuItem(dock.Name.c_str(), nullptr, &dock.IsOpen);
+    //   }
+    //   ImGui::EndMenu();
+    // }
+
+    ImGui::EndMenuBar();
+  }
+
+  ImGui::End();
+
+  // for (auto& dock : Docks) {
+  //   dock.Show();
+  // }
+}
+
 int
 main(int argc, char** argv)
 {
@@ -132,6 +295,67 @@ main(int argc, char** argv)
     std::cout << "Failed to initialize GLAD" << std::endl;
     return 3;
   }
+
+  glfwSwapInterval(1); // Enable vsync
+
+  // Setup Dear ImGui context
+  IMGUI_CHECKVERSION();
+  ImGui::CreateContext();
+  ImGuiIO& io = ImGui::GetIO();
+  (void)io;
+  io.ConfigFlags |=
+    ImGuiConfigFlags_NavEnableKeyboard; // Enable Keyboard Controls
+  io.ConfigFlags |=
+    ImGuiConfigFlags_NavEnableGamepad;                // Enable Gamepad Controls
+  io.ConfigFlags |= ImGuiConfigFlags_DockingEnable;   // Enable Docking
+  io.ConfigFlags |= ImGuiConfigFlags_ViewportsEnable; // Enable Multi-Viewport /
+                                                      // Platform Windows
+  // io.ConfigViewportsNoAutoMerge = true;
+  // io.ConfigViewportsNoTaskBarIcon = true;
+
+  // Setup Dear ImGui style
+  ImGui::StyleColorsDark();
+  // ImGui::StyleColorsLight();
+
+  // When viewports are enabled we tweak WindowRounding/WindowBg so platform
+  // windows can look identical to regular ones.
+  ImGuiStyle& style = ImGui::GetStyle();
+  if (io.ConfigFlags & ImGuiConfigFlags_ViewportsEnable) {
+    style.WindowRounding = 0.0f;
+    style.Colors[ImGuiCol_WindowBg].w = 1.0f;
+  }
+
+  // Setup Platform/Renderer backends
+  ImGui_ImplGlfw_InitForOpenGL(window, true);
+  ImGui_ImplOpenGL3_Init("#version 130");
+
+  // Load Fonts
+  // - If no fonts are loaded, dear imgui will use the default font. You can
+  // also load multiple fonts and use ImGui::PushFont()/PopFont() to select
+  // them.
+  // - AddFontFromFileTTF() will return the ImFont* so you can store it if you
+  // need to select the font among multiple.
+  // - If the file cannot be loaded, the function will return a nullptr. Please
+  // handle those errors in your application (e.g. use an assertion, or display
+  // an error and quit).
+  // - The fonts will be rasterized at a given size (w/ oversampling) and stored
+  // into a texture when calling ImFontAtlas::Build()/GetTexDataAsXXXX(), which
+  // ImGui_ImplXXXX_NewFrame below will call.
+  // - Use '#define IMGUI_ENABLE_FREETYPE' in your imconfig file to use Freetype
+  // for higher quality font rendering.
+  // - Read 'docs/FONTS.md' for more instructions and details.
+  // - Remember that in C/C++ if you want to include a backslash \ in a string
+  // literal you need to write a double backslash \\ !
+  // - Our Emscripten build process allows embedding fonts to be accessible at
+  // runtime from the "fonts/" folder. See Makefile.emscripten for details.
+  // io.Fonts->AddFontDefault();
+  // io.Fonts->AddFontFromFileTTF("c:\\Windows\\Fonts\\segoeui.ttf", 18.0f);
+  // io.Fonts->AddFontFromFileTTF("../../misc/fonts/DroidSans.ttf", 16.0f);
+  // io.Fonts->AddFontFromFileTTF("../../misc/fonts/Roboto-Medium.ttf", 16.0f);
+  // io.Fonts->AddFontFromFileTTF("../../misc/fonts/Cousine-Regular.ttf", 15.0f);
+  // ImFont* font =
+  // io.Fonts->AddFontFromFileTTF("c:\\Windows\\Fonts\\ArialUni.ttf", 18.0f,
+  // nullptr, io.Fonts->GetGlyphRangesJapanese()); IM_ASSERT(font != nullptr);
 
   // configure global opengl state
   // -----------------------------
@@ -249,6 +473,20 @@ void main()
   // render loop
   // -----------
   while (!glfwWindowShouldClose(window)) {
+    glfwPollEvents();
+
+    // Start the Dear ImGui frame
+    ImGui_ImplOpenGL3_NewFrame();
+    ImGui_ImplGlfw_NewFrame();
+    ImGui::NewFrame();
+
+    DockSpace();
+
+    // 1. Show the big demo window (Most of the sample code is in
+    // ImGui::ShowDemoWindow()! You can browse its code to learn more about Dear
+    // ImGui!).
+    ImGui::ShowDemoWindow();
+
     processInput(window);
     int width, height;
     glfwGetFramebufferSize(window, &width, &height);
@@ -303,13 +541,33 @@ void main()
       renderQuad();
     }
 
+    ImGui::Render();
+    ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
+
+    // Update and Render additional Platform Windows
+    // (Platform functions may change the current OpenGL context, so we
+    // save/restore it to make it easier to paste this code elsewhere.
+    //  For this specific demo app we could also call
+    //  glfwMakeContextCurrent(window) directly)
+    if (io.ConfigFlags & ImGuiConfigFlags_ViewportsEnable) {
+      GLFWwindow* backup_current_context = glfwGetCurrentContext();
+      ImGui::UpdatePlatformWindows();
+      ImGui::RenderPlatformWindowsDefault();
+      glfwMakeContextCurrent(backup_current_context);
+    }
+
     // glfw: swap buffers and poll IO events (keys pressed/released, mouse moved
     // etc.)
     // -------------------------------------------------------------------------------
     glfwSwapBuffers(window);
-    glfwPollEvents();
   }
 
+  // Cleanup
+  ImGui_ImplOpenGL3_Shutdown();
+  ImGui_ImplGlfw_Shutdown();
+  ImGui::DestroyContext();
+
+  glfwDestroyWindow(window);
   glfwTerminate();
   return 0;
 }
